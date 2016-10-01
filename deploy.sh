571a4ce3a22fb662acd0e8f1aead757b5eb8de97
Larry Ogrodnek <ogrodnek@gmail.com>
2016-10-01 16:44:00 -0700

    add cache control headers

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 R=`dirname $0`
 
 rm -rf $R/.build
-cactus build && aws s3 sync $R/.build s3://sandwichnews.org
+cactus build && aws s3 sync $R/.build s3://sandwichnews.org --profile analog --cache-control max-age=900
