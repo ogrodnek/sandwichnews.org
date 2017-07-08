aa4a969979302042ecb13ee016dfc9df03f1fccf
Larry Ogrodnek <ogrodnek@gmail.com>
2017-07-08 00:25:52 -0700

    add CF invalidation

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,24 @@
 R=`dirname $0`
 
 rm -rf $R/.build
-cactus build && aws s3 sync $R/.build s3://sandwichnews.org --profile analog --cache-control max-age=900
+cactus build
+aws s3 sync $R/.build s3://sandwichnews.org --profile analog --cache-control max-age=900
+
+#CF Invalidation
+INVALIDATION_ID=$(date +"%s")
+INVALIDATION_JSON="{
+    \"DistributionId\": \"E12B42DU84I3C\",
+    \"InvalidationBatch\": {
+        \"Paths\": {
+            \"Quantity\": 3,
+            \"Items\": [
+                \"/rss.xml\",
+                \"/index.html\",
+		\"/\"
+            ]
+        },
+        \"CallerReference\": \"$INVALIDATION_ID\"
+    }
+}"
+
+aws cloudfront create-invalidation --cli-input-json "$INVALIDATION_JSON" --profile analog
