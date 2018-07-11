cfb7b9fca8fab33d88024825181b9569f79e5800
Larry Ogrodnek <ogrodnek@gmail.com>
2018-07-11 11:01:39 -0700

    longer cache for jpg

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,7 @@ fi
 
 R=`dirname $0`
 
-aws s3 sync $R/build s3://${S3_DEPLOY_BUCKET} --cache-control max-age=900
+aws s3 sync $R/build s3://${S3_DEPLOY_BUCKET} --exclude "*.jpg" --cache-control max-age=900
+aws s3 sync $R/build s3://${S3_DEPLOY_BUCKET} --include "*.jpg" --cache-control max-age=7200
+
 aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths '/*'
