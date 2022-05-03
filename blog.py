49f5c6a510435cddd4b21c8e5b89ae19e2288023
Larry Ogrodnek <ogrodnek@gmail.com>
2022-05-02 20:17:32 -0400

    formatting

--- blog.py
+++ blog.py
@@ -7,67 +7,76 @@ from mdx_imagecaption import ImageCaptionExtension
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
-FLATPAGES_EXTENSION = '.md'
+FLATPAGES_EXTENSION = ".md"
 FLATPAGES_EXTENSION_CONFIGS = {
-    'codehilite': {},
-    'fenced_code': {},
+    "codehilite": {},
+    "fenced_code": {},
 }
 
-FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite', 'fenced_code',  ImageCaptionExtension()]
+FLATPAGES_MARKDOWN_EXTENSIONS = ["codehilite", "fenced_code", ImageCaptionExtension()]
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
 freezer = Freezer(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     tags = set()
     for p in pages:
-      for t in p.meta.get('tags', []):
-        tags.add(t)
-    
+        for t in p.meta.get("tags", []):
+            tags.add(t)
+
     _s = sorted(tags)
 
-    latest = sorted(pages, reverse=True, key=lambda p: str(p.meta['date']))
-    return render_template('index.html', pages=latest, tags=_s)
+    latest = sorted(pages, reverse=True, key=lambda p: str(p.meta["date"]))
+    return render_template("index.html", pages=latest, tags=_s)
 
 
-@app.route('/rss.xml')
+@app.route("/rss.xml")
 def rss():
-  latest = sorted(pages, reverse=True, key=lambda p: str(p.meta['date']))
-  return render_template('rss.xml', posts=latest), 200, { 'Content-Type' : "application/xml" }
+    latest = sorted(pages, reverse=True, key=lambda p: str(p.meta["date"]))
+    return (
+        render_template("rss.xml", posts=latest),
+        200,
+        {"Content-Type": "application/xml"},
+    )
 
-@app.route('/tag/<string:tag>/')
+
+@app.route("/tag/<string:tag>/")
 def tag(tag):
-    _tagged = [p for p in pages if tag in p.meta.get('tags', [])]
-    tagged = sorted(_tagged, key=lambda p: p.meta['date'], reverse=True)
+    _tagged = [p for p in pages if tag in p.meta.get("tags", [])]
+    tagged = sorted(_tagged, key=lambda p: p.meta["date"], reverse=True)
 
-    return render_template('tag.html', pages=tagged, tag=tag)
+    return render_template("tag.html", pages=tagged, tag=tag)
+
 
 def related(page):
-  def find():
-    rel = {}
-    for p in pages:
-      if p.path == page.path:
-        continue
-      for t in page.meta.get('tags', []):
-        if t in p.meta.get('tags', []):
-          rel[p.path] = p
-    return rel
-  
-  rel = find().values()
+    def find():
+        rel = {}
+        for p in pages:
+            if p.path == page.path:
+                continue
+            for t in page.meta.get("tags", []):
+                if t in p.meta.get("tags", []):
+                    rel[p.path] = p
+        return rel
 
-  return sorted(rel, key=lambda p: p.meta['date'], reverse=True)
+    rel = find().values()
 
-@app.route('/<path:path>.html')
+    return sorted(rel, key=lambda p: p.meta["date"], reverse=True)
+
+
+@app.route("/<path:path>.html")
 def page(path):
     page = pages.get_or_404(path)
     r = related(page)
 
-    return render_template('page.html', page=page, related=r)
+    return render_template("page.html", page=page, related=r)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     if len(sys.argv) > 1 and (sys.argv[1] == "build" or sys.argv[1] == "freeze"):
         freezer.freeze()
     else:
